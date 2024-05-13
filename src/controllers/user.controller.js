f007f766fab75307e8ad8548fb499dfa0c99c9f3
Ankit <av542939@gmail.com>
2024-05-13 16:23:29 +0530

    bug fixes in login

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -119,7 +119,7 @@ const loginUser = asyncHandler(async (req, res) =>{
 
    const {accessToken, refreshToken} = await generateAccessAndRefreshToken(user._id)
 
-   const loggedInUser = User.findById(user._id).select("-password -refreshToken")
+   const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
 
    const options = {
     httpOnly : true,
